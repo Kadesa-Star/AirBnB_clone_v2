775efaef62c3a0860d60186b6b81415422dfc303
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-16 18:39:54 +0300

    prepare your webservers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -30,5 +30,29 @@ fi
 # Change ownership of /data/ directory recursively to ubuntu user and group
 chown -R ubuntu:ubuntu /data/
 
+# Configure nginx to serve the content
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By \$HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
 # Restart nginx service
 service nginx restart
